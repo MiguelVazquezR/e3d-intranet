50d27ea3748cefefe10e50d94f0cee6936becd87
Miguel Vázquez <72841291+MiguelVazquezR@users.noreply.github.com>
2023-04-07 15:31:12 -0600

    Add files via upload

---
+++ database/migrations/2023_04_05_135206_create_maintenances_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaintenancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            
+            $table->text('problems');
+            $table->text('actions');
+            $table->unsignedFloat('cost');
+            $table->enum('maintenance_type',[0,1]);
+            $table->string('responsible');
+            $table->foreignId('machine_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenances');
+    }
+}
